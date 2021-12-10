bd026ab7ab214cef2a9770bbd8e24276f56bd4e7
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2021-12-11 00:15:41 +0400

    here need swapp value

---
+++ SwappingValues.js
@@ -0,0 +1,11 @@
+// Swapping Values using the Destructuring Assignment
+let a = 3;
+let b = 6;
+let c = a;
+ a = b;
+ b = c;
+
+console.log(a); //6
+console.log(b); //3
+
+// i think im make mistake here, this is not real solution :D 
